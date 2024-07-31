42e0a7a7326c085b63e8593e2d0c1d18c9687965
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-07-31 03:37:40 +0000

    final

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const currentTime = document.querySelector("h1"),
-content = document.querySelector("content"),
+content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
+
 let alarmTime, isAlarmSet,
-ringtone = new Audio("files/alarm.mp3");
+ringtone = new Audio("alarm.mp3");
+
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? "0" + i : i;
     let option = `<option value="${i}">${i}</option>`;
@@ -19,6 +21,7 @@ for (let i = 2; i > 0; i--) {
     let option = `<option value="${ampm}">${ampm}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
 setInterval(() => {
     let date = new Date(),
     h = date.getHours(),
